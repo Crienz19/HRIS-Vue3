2a3674317bbdfd668a537d6dea2937a03715bce2
Renz Mergenio <zner.mergenio@gmail.com>
2021-11-19 11:21:26 +0800

    Adding new api header

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,10 +1,11 @@
 import axios, { AxiosInstance } from 'axios';
 
 const http: AxiosInstance = axios.create({
-    baseURL: 'https://hr.ziptravel.com.ph/v1',
+    baseURL: 'https://hr.ziptravel.com.ph',
     withCredentials: true,
     headers: {
         'Accept': 'application/json',
+        'Content-Type': 'application/json',
     }
 });
 
